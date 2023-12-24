cf4a7684706d61c6146b415aa36ece9ca3266504
Dl0rd <124901949+Dl0rd@users.noreply.github.com>
2023-12-24 23:48:45 +0700

    v1.0.6 fix interface error

--- interface.py
+++ interface.py
@@ -1,5 +1,7 @@
 import flet as ft
 
+import re
+
 import pyautogui as pg
 import pyperclip as pc
 
@@ -36,21 +38,33 @@ class PasswordSetting(ft.UserControl):
 
         self.input_password_range = ft.TextField(label="Диапозон паролей (1, 2, 3, 4 или '2-4')", icon=ft.icons.LOCK,expand=True)
     
-    def get_setting_value(self):
-        return self.input_password_range.value,self.c1.value,self.c2.value,self.c3.value,self.c4.value
+    async def get_setting_value(self):
+        val = self.input_password_range.value
+        self.input_password_range.error_text = None
+        if re.search(r"[a-zA-Zа-яА-я(){}]",val) is None:
+            await self.input_password_range.update_async()
+            return val,self.c1.value,self.c2.value,self.c3.value,self.c4.value            
+        else:
+            self.input_password_range.error_text = 'Неправильное значение'
+            await self.input_password_range.update_async()
+            return False,
+    async def all_checkbox(self,e,status):
+        for c in (self.c1,self.c2,self.c3,self.c4):
+            if c.disabled is not status:
+                c.disabled = status
+            await c.update_async()
 
     async def change_checkbox(self,e):
         if (self.c1.value + self.c2.value + self.c3.value + self.c4.value) == 1:
             for c in (self.c1,self.c2, self.c3, self.c4):
                 if c.value == True:
                     c.disabled = True
-                    await c.update_async()
+                await c.update_async()
         else:
             for c in (self.c1,self.c2,self.c3,self.c4):
                 c.disabled = False
                 await c.update_async()
         await self.page.update_async()
-
     def build(self):
         return Frame(self.page,
         [
@@ -103,9 +117,40 @@ class PositonSetting(ft.UserControl):
             self.banner.content.value = F"Выбрана точка: {self.pos}"
             await self.banner.update_async()
             await self.open_banner(e)
-    def get_positions(self):
-        return self.pos1.value,self.pos2.value
 
+    async def get_positions(self):
+        pos1 = self.pos1.current.value
+        pos2 = self.pos2.current.value
+        x1 = y1 = x2 = y2 = None
+        if re.search(r"[a-zA-Zа-яА-я]",pos1) is None:
+            self.pos1.current.error_text = None
+            try:
+                x1,y1 = pos1.split(', ')
+            except ValueError:
+                self.pos1.current.error_text = 'Неправильное значение'
+                await self.pos1.current.update_async()
+        else: 
+            self.pos1.current.error_text = 'Неправильное значение'
+            await self.pos1.current.update_async()
+            return False,
+        if re.search(r"[a-zA-Zа-яА-я]",pos2) is None:
+            self.pos2.current.error_text = None
+            try:
+                x2,y2 = pos1.split(', ')
+            except ValueError:
+                self.pos2.current.error_text = 'Неправильное значение'
+                await self.pos2.current.update_async()
+        else: 
+            self.pos1.current.error_text = 'Неправильное значение'    
+            await self.pos2.current.update_async()
+            return False,
+        try:
+            return (int(x1[1:]),int(y1[:-1])),(int(x2[1:]),int(y2[:-1]))
+        except ValueError:
+            self.pos2.current.error_text = 'Неправильное значение'
+            await self.pos2.current.update_async()
+            self.pos1.current.error_text = 'Неправильное значение'
+            await self.pos1.current.update_async()
     def build(self):
         return Frame(self.page,
                 obj=[
@@ -153,31 +198,33 @@ class SearchPassword(ft.UserControl):
             self.position_setting.pos2.current.error_text = 'Обязательно для заполнения'
             await self.position_setting.pos2.current.update_async()
         else:
-            self.status_search = False if self.status_search else True
-            if self.status_search:
-                self.pb.value = 0
-                self.col_pb.controls.insert(0,self.text_above_pb)
-                await self.col_pb.update_async()
-                await self.pb.update_async()
-            else:
-                self.pb.value = None
-                self.col_pb.controls.remove(self.text_above_pb)
-                await self.col_pb.update_async()
-                await self.pb.update_async()
+            setting_password = await self.password_setting.get_setting_value()
+            position_setting = await self.position_setting.get_positions()
+            if setting_password[0] is not False and position_setting[0] is not False:
+                self.status_search = False if self.status_search else True
+                if self.status_search:
+                    self.pb.value = 0
+                    self.col_pb.controls.insert(0,self.text_above_pb)
+                    await self.col_pb.update_async()
+                    await self.pb.update_async()
+                    if any(setting_password[1:]) is False:
+                        self.password_setting.c1.value = True
+                        await self.password_setting.c1.update_async()
+                else:
+                    self.pb.value = None
+                    self.col_pb.controls.remove(self.text_above_pb)
+                    await self.col_pb.update_async()
+                    await self.pb.update_async()
+                self.position_setting.pos1.current.disabled = False if self.position_setting.pos1.current.disabled else True
+                self.position_setting.pos2.current.disabled = False if self.position_setting.pos2.current.disabled else True
+                
+                await self.password_setting.all_checkbox(e,self.status_search)
 
-            self.position_setting.pos1.current.disabled = False if self.position_setting.pos1.current.disabled else True
-            self.position_setting.pos2.current.disabled = False if self.position_setting.pos2.current.disabled else True
-
-            self.password_setting.c1.disabled = False if self.password_setting.c1.disabled else True 
-            self.password_setting.c2.disabled = False if self.password_setting.c2.disabled else True 
-            self.password_setting.c3.disabled = False if self.password_setting.c3.disabled else True 
-            self.password_setting.c4.disabled = False if self.password_setting.c4.disabled else True 
-            await self.password_setting.c1.update_async() 
-            await self.password_setting.c2.update_async()
-            await self.password_setting.c3.update_async()
-            await self.password_setting.c4.update_async()
-            self.password_setting.input_password_range.disabled = False if self.password_setting.input_password_range.disabled else True
-        
+                await self.password_setting.c1.update_async() 
+                await self.password_setting.c2.update_async()
+                await self.password_setting.c3.update_async()
+                await self.password_setting.c4.update_async()
+                self.password_setting.input_password_range.disabled = False if self.password_setting.input_password_range.disabled else True
         await self.position_setting.pos1.current.update_async()
         await self.position_setting.pos2.current.update_async()
         await self.password_setting.input_password_range.update_async()
@@ -192,7 +239,7 @@ class SearchPassword(ft.UserControl):
             label_icon=ft.icons.SEARCH)
 async def main(page:ft.Page):
    
-    page.window_height = 850
+    page.window_height = 800
     page.window_width = 500
     page.window_resizable = False
     page.title = 'LAND пароли'
